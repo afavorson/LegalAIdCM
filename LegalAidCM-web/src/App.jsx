ba8281a9abc61694ce35e0262b55250546d65a37
afavorson <afavorson@gmail.com>
2025-01-17 19:51:22 +0100

    Navbar created

--- LegalAidCM-web/src/App.jsx
+++ LegalAidCM-web/src/App.jsx
@@ -1,15 +1,29 @@
 
 import './App.css'
+// import Logo from './assets/MLogo.png';
 
 function App() {
   
 
   return (
     <>
-      <h1>LegalAidCM</h1>
-      <h4>Quick legal first aid</h4>
+      <div id="main">
+        <div id="navbar">
+          <a href="#" id="Logo">LegalAidCM</a>
+          <ul>
+            <li>Home</li>
+            <li>Emergency Advice</li>
+            <li>Legal Directory</li>
+            <li>Incident Reporting</li>
+          </ul>
+          <div id="profile">
+            <img src="../nodir/profile.png" alt="Profile" />
+          </div>
+        </div>
+
+      </div>
     </>
-  )
+  );
 }
 
 export default App
